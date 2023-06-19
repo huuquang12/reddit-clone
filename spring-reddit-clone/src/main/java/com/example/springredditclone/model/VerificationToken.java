16786730253b51420b9aefcca55378b2c0d78c37
quangdh <h.quang1200@gmail.com>
2023-06-19 23:16:11 +0700

    create user and post model

---
+++ spring-reddit-clone/src/main/java/com/example/springredditclone/model/VerificationToken.java
@@ -0,0 +1,24 @@
+package com.example.springredditclone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Entity
+@Table(name = "token")
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
+}
